c3a63f2c0c27c3bb920a46a0f59481421a36fe57
gabe-man <gabrielmorales1996@gmail.com>
2021-12-02 14:30:48 +0100

    Creo una calculadora

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,31 @@
+package Calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=0;
+		int y=0;
+		int resultado=0;
+		String opc=null;
+		System.out.println("x: ");
+		x=Integer.parseInt(sc.nextLine());
+		System.out.println("y: ");
+		y=Integer.parseInt(sc.nextLine());
+		System.out.println("¿Quiere sumar o restar?");
+		opc=sc.nextLine();
+		if(opc.equals("sumar"))
+		{
+			resultado=x+y;
+		}
+		if(opc.equals("restar"))
+		{
+			resultado=x-y;
+		}
+		System.out.println(resultado);
+
+	}
+
+}
